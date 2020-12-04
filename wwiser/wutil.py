1ece729a0d875cc818a8491e73e1b8faba53436d
bnnm <bananaman255@gmail.com>
2020-12-04 23:43:58 +0100

    generator: slighly improve search perfomance

--- wwiser/wutil.py
+++ wwiser/wutil.py
@@ -105,7 +105,11 @@ class NodeFinder(object):
             return None
         self.first = first
         self.depth = 0
-        self._find(node)
+
+        # aim for outer nodes first as it's slightly faster in some cases
+        #self._find_inner(node)
+        self._find_outer([node])
+
         if self.results:
             if len(self.results) > 1:
                 raise ValueError("more than 1 result found")
@@ -117,63 +121,108 @@ class NodeFinder(object):
         if not node:
             return []
         self.depth = 0
-        self._find(node)
+
+        # aim for outer nodes first as it's slightly faster in some cases
+        #self._find_inner(node)
+        self._find_outer([node])
+
         return self.results
 
-    def _find(self, node):
+    # find query in tree, going in depth first:
+    # A > B > C
+    #         > D
+    #       > E
+    #         > F
+    #   > G
+    # order: A B C D E F G (finds lower nodes faster)
+    def _find_inner(self, node):
         if self.empty:
             return
         if self.first and self.results:
             return
 
-        #attrs = node.get_attrs()
-        children = node.get_children()
-
-        #todo may simplify with a list of find key + value (like contains)
-        # find target values in attrs
-        ignore = self.depth > 0 or self.depth == 0 and self.base #first
-        if ignore:
-            attr = node.get_attr('name')
-            #if 'name' in attrs:
-            if attr:
-                for target in self.names:
-                    if attr == target:
-                    #if attrs['name'] == target:
-                        self.results.append(node)
-
-            attr = node.get_attr('type')
-            if attr:
-            #if 'type' in attrs:
-                for target in self.types:
-                    #if attrs['type'] == target:
-                    if attr == target:
-                        self.results.append(node)
-
-            attr = node.get_attr('value')
-            if attr is not None:
-            #if 'value' in attrs:
-                for target in self.values:
-                    if attr == target:
-                    #if attrs['value'] == target:
-                        self.results.append(node)
-            if self.contains:
-                key, val = self.contains
-                attr = node.get_attr(key)
-                if attr and val in attr:
-                #if key in attrs and val in attrs[key]:
-                    self.results.append(node)
-
+        self._query(node)
 
         # target exists and only need one result: stop
         if self.first and self.results:
             return
 
         # keep finding results in children
-        if children:
-            for subnode in children:
-                self.depth += 1
-                self._find(subnode)
-                self.depth -= 1
+        children = node.get_children()
+        if not children:
+            return
+        for subnode in children:
+            self.depth += 1
+            self._find_inner(subnode)
+            self.depth -= 1
+
+    # find query in tree, going same-level first:
+    # A > B > C
+    #         > D
+    #       > E
+    #         > F
+    #   > G
+    # order: A B G C E D F (finds upper nodes faster)
+    def _find_outer(self, nodes):
+        if self.empty:
+            return
+        if self.first and self.results:
+            return
+
+        if not nodes:
+            return
+
+        # find query on this level
+        for node in nodes:
+            self._query(node)
+            # target exists and only need one result: stop
+            if self.first and self.results:
+                return
+
+        # find query on children level
+        for node in nodes:
+            self.depth += 1
+            self._find_outer(node.get_children())
+            self.depth -= 1
+            # target exists and only need one result: stop
+            if self.first and self.results:
+                return
+
+
+    def _query(self, node):
+        #todo may simplify with a list of find key + value (like contains)
+        # find target values in attrs
+        #attrs = node.get_attrs()
+        valid = self.depth > 0 or self.depth == 0 and self.base #first
+        if not valid:
+            return
+
+        attr = node.get_attr('name')
+        if attr:
+            for target in self.names:
+                if attr == target:
+                    self.results.append(node)
+
+        attr = node.get_attr('type')
+        if attr:
+            for target in self.types:
+                if attr == target:
+                    self.results.append(node)
+
+        attr = node.get_attr('value')
+        if attr is not None:
+            for target in self.values:
+                if attr == target:
+                    self.results.append(node)
+
+        if self.contains:
+            key, val = self.contains
+            attr = node.get_attr(key)
+            if attr and val in attr:
+                self.results.append(node)
+
+        return
+
 
 def node_find(node, name=None, type=None, names=None, types=None, value=None, values=None):
     finder = NodeFinder(name=name, type=type, names=names, types=types)

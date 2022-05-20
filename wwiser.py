6dc57b29c56df55bcf15c6082f1599c3338b880a
bnnm <bananaman255@gmail.com>
2022-05-20 22:03:03 +0200

    profile debug

--- wwiser.py
+++ wwiser.py
@@ -7,11 +7,42 @@ import sys
 from wwiser import wcli
 from wwiser import wgui
 
-if __name__ == "__main__":
-    #import profile
-    #profile.run('wcli.Cli().start()')
+_PROFILE = False
 
+
+def main():
     if len(sys.argv) > 1:
         wcli.Cli().start()
     else:
         wgui.Gui().start()
+
+
+def profile_simple():
+    try:
+        import cProfile as profile
+    except:
+        import profile
+    profile.run('wcli.Cli().start()')
+
+
+def profile_complex():
+    import pstats
+    try:
+        import cProfile as profile
+    except:
+        import profile
+    #profile.run('wcli.Cli().start()')
+    profiler = profile.Profile()
+    profiler.enable()
+    main()
+    profiler.disable()
+    sort = 'cumtime' #tottime ncalls
+    stats = pstats.Stats(profiler).sort_stats(sort)
+    stats.print_stats()
+
+
+if __name__ == "__main__":
+    if _PROFILE:
+        profile_complex()
+    else:
+        main()
